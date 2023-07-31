2bdb50b88e5324a46586783503eac6bbe76e900b
Jasper Javillo <jasperjavillo@gmail.com>
2023-07-31 11:17:21 -0400

    Created capstone-order project

---
+++ capstone-order/src/test/java/com/capstone/capstoneorder/CapstoneOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.capstoneorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
